2634b191b440ab8bc900c08b4ca7c6840574adc8
s179071 <s179071@SEA-8FE212>
2017-08-14 12:45:58 -0700

    Add project files.

---
+++ 1-HelloAIE/main.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+
+int main() 
+{
+	//Closed 1
+	std::cout << "Hello AIE" << std::endl;
+
+	system("pause");
+
+	//Closed 2 -- soundtracks
+	std::cout << "A list of my favorite tracks:" << std::endl;
+	std::cout << "1 -- FF13-3 OST - City of Revelry" << std::endl;
+	std::cout << "2 -- Moleman - Skyline" << std::endl;
+	std::cout << "3 -- Flight Facilities - Clair De Lune (Motez remix)" << std::endl;
+
+	system("pause");
+
+	//Closed 3 & 4 -- 0 to 5 and 0 to 5 to 0
+	std::cout << std::endl;
+	std::cout << "0" << std::endl;
+	std::cout << "1" << std::endl;
+	std::cout << "2" << std::endl;
+	std::cout << "3" << std::endl;
+	std::cout << "4" << std::endl;
+	std::cout << "5" << std::endl;
+	std::cout << "4" << std::endl;
+	std::cout << "3" << std::endl;
+	std::cout << "2" << std::endl;
+	std::cout << "1" << std::endl;
+	std::cout << "0" << std::endl;
+	std::cout <<  std::endl;
+
+	system("pause");
+	
+	// open 1 
+	std::cout << "SyntacticalErrors-A)" << std::endl;
+	std::cout << "Howdy AIE" << std::endl;
+	std::cout << "It's a fine day today as any!" << std::endl; //no semi-colon at the end of this line
+	std::cout << "I really wish I had some potatoes..." << std::endl;
+
+	system("pause");
+	
+	//open 2
+	std::cout << "SyntacticalErrors-B)" << std::endl;
+	std::cout << "Four score and seven years ago" << std::endl;
+	std::cout << "our fathers brought forth on this continent," << std::endl;
+	std::cout << "a new nation," << std::endl;
+	std::cout << "conceived in Libery, and dedicated to the proposition" << std::endl;
+	std::cout << "that all men are created equal." << std::endl;
+	std::cout << "Now we are engaged in a great civil war," << std::endl;
+	std::cout << "testing whether that nation, or any nation" << std::endl; // no 2nd quote after nation on this line
+	std::cout << "so conceived and so dedicated, can long endure." << std::endl;
+
+	system("pause");
+
+	/* 
+	THIS
+	COMMENT
+	IS
+	NOT
+	NECCESSARY
+	*/
+}
